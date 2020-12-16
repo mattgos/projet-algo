4fa32d2fd7ccb6794d0dc12760c035106579c6d7
dmanou <75978474+dmanou@users.noreply.github.com>
2020-12-16 10:59:24 +0100

    Update lottery Rcpp
    Modif sortie + commentaires

--- src/lottery_rcpp.cpp
+++ src/lottery_rcpp.cpp
@@ -1,6 +1,12 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+//' Which algorithme using C++
+//' 
+//' @param v un vector
+//' @param nombre un nombre
+//' @return renvoie l'indice du vecteur où v[ind] = nombre (sinon renvoie -1)
+//' @export 
 // [[Rcpp::export]]
 int which_is_equal(IntegerVector v, int nombre) {
   for(int i = 0; i < v.length(); i++) {
@@ -11,6 +17,12 @@ int which_is_equal(IntegerVector v, int nombre) {
   return(-1);
 }
 
+//' is_element algorithme using C++
+//' 
+//' @param v un vector
+//' @param n un nombre
+//' @return renvoie si n appartient à v ou non
+//' @export
 // [[Rcpp::export]]
 bool is_element(int n, IntegerVector v) {
   for(int i = 0; i < v.size(); i++) {
@@ -21,6 +33,13 @@ bool is_element(int n, IntegerVector v) {
   return(false);
 }
 
+//' Which algorithme using C++
+//' 
+//' @param P une liste de projet avec des équipes de projet
+//' @param G une liste de groupe d'étudiant
+//' @param R une liste de voeu classée par ordre de priorité pour chaque groupe
+//' @return renvoie une liste X pour chaque équipe projet remplie ou non par des groupes
+//' @export
 // [[Rcpp::export]]
 List lottery_rcpp(List P, List G, List R) {
   List P_copy(P.size());
@@ -30,6 +49,11 @@ List lottery_rcpp(List P, List G, List R) {
   IntegerVector numero_groupe = seq(1,G.size());
   IntegerVector numero_projet = seq(1,P.size());
   List X(P.size());
+  for(int x = 0; x < X.size(); x++) {
+    List pj = P[x];
+    List x_pj(pj.size());
+    X[x] = x_pj;
+  }
   int h = 0;
   int nb_groupe = numero_groupe.length();
   while(nb_groupe != 0 && h < P.size()){
@@ -56,12 +80,14 @@ List lottery_rcpp(List P, List G, List R) {
           int groupe_choisi = c[k];
           IntegerVector projet_equipe = P_copy[i];
           for(int j = 0; j < projet_equipe.length(); j++) {
+            List equipe = x_pj[j];
             IntegerVector groupe_eleve = G[groupe_choisi - 1];
             int temporaire = projet_equipe[j] - groupe_eleve.length();
             if (temporaire >= 0) {
               projet_equipe[j] = temporaire;
               P_copy[i] = projet_equipe;
-              x_pj.push_back(groupe_choisi);
+              equipe.push_back(groupe_choisi);
+              x_pj[j] = equipe;
               int indice = which_is_equal(numero_groupe,groupe_choisi);
               numero_groupe.erase(indice);
               break;

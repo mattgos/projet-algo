0150041950ab90aee25ba924b919fb771ec6c7bc
Matthieu <matthieu@pc60.home>
2021-01-08 22:16:24 +0100

    add fct simulation

---
+++ R/simu.R
@@ -0,0 +1,41 @@
+simu<-function(nb_eleve,nb_proj){
+  
+  cap_proj<-nb_eleve%/%nb_proj
+  reste<-nb_eleve%%nb_proj
+  P<-rep(cap_proj,nb_proj)
+  ind_aj<-sample(1:nb_proj,size=reste)
+  P[ind_aj]=P[ind_aj]+1
+  
+  repart_eleve=1:nb_eleve
+  nb_group=sample(nb_proj:nb_eleve,1)
+  prem=sample(repart_eleve,nb_group)
+  G=list()
+  for (i in prem){
+    G=append(G,i)
+    
+  }
+  
+  repart_eleve=repart_eleve[!(repart_eleve %in% prem)]
+  
+  while (length(repart_eleve)>reste){
+    eleve=sample(repart_eleve,1)
+    groupe=sample(1:nb_group,1)
+    if (length(G[[groupe]])<cap_proj){
+      G[[groupe]]=c(G[[groupe]],eleve)
+    }
+    repart_eleve=repart_eleve[!(repart_eleve==eleve)]
+  }
+  ind_aj<-sample(1:nb_group,size=reste)
+  j=1
+  for (i in repart_eleve){
+    G[[ind_aj[j]]]=c(G[[ind_aj[j]]],i)
+    j=j+1
+  }
+  
+  R=matrix(0,ncol=nb_proj,nrow=nb_group)
+  for (i in 1:nb_group){
+    R[i,]=sample(1:nb_proj)
+  }
+  
+  return(list(P=P,G=G,R=R))
+}

507455552e67276c3d4583faafe60b2e7c79db14
Matthieu <matthieu@pc107.home>
2021-01-16 13:55:05 +0100

    modif arg et res lotterieR

--- R/lotterie.R
+++ R/lotterie.R
@@ -1,9 +1,32 @@
-#P=c("projet 1","projet 2","projet 3")
-#p=list(c(2,2),c(3),c(3,2))
-#G=list(c(1,5,3),c(2,6),c(4),c(7,10),c(8),c(9))
-#R=list(c(1,3),c(2,3,1),c(3),c(1,2,3),c(2,1),c(1,3))
+# P=c("projet 1","projet 2","projet 3")
+# p=list(c(2,2),c(3),c(3,2))
+# G=list(c(1,5,3),c(2,6),c(4),c(7,10),c(8),c(9))
+# R=list(c(1,3),c(2,3,1),c(3),c(1,2,3),c(2,1),c(1,3))
+# 
+# P=c("projet 1","projet 2")
+# p=list(c(3),c(3))
+# G=list(c(4,2),c(3),c(5),c(1),c(6))
+# R=list(c(2,1),c(1,2),c(1,2),c(2,1),c(2,1))
 
-lotterie<-function(P,p,G,R){
+lotterie<-function(G,P,R){
+  
+  p=list()
+  for (i in P){
+    p=append(p,c(i))
+  }
+
+  R_new=R
+  R=list()
+  for (i in 1:length(R_new[,1])){
+    R=append(R,list(R_new[i,]))
+  }
+
+  P_new=P
+  P=c()
+  for (i in 1:length(P_new)){
+    P=c(P,paste("projet",i))
+  }
+  
   ind_group=1:length(G)
   nb_proj=length(P)
   nb_gr=length(G)
@@ -20,7 +43,7 @@ lotterie<-function(P,p,G,R){
   Y=NULL
   while (length(ind_group)!=0){
     #liste C
-    C=list(rep(0,nb_proj))
+    C=list(rep(list(),nb_proj))
     for (i in 1:nb_proj){
       c=c()
       for (j in ind_group){
@@ -28,12 +51,12 @@ lotterie<-function(P,p,G,R){
           if (R[[j]][h]==i) c=c(c,j)
         }
       }
-
       C[[i]]=c
     }
-
+    
     #boucle attribution
     for (i in 1:nb_proj){
+      if (i<=length(C)){
       if (length(C[[i]])!=0){
         c_pi=C[[i]]
         if (length(c_pi)>1){c_pi=sample(c_pi)}
@@ -61,6 +84,7 @@ lotterie<-function(P,p,G,R){
           
           if (is.element(j,ind_group) & h==length(R[[j]])) ind_group=ind_group[ind_group!=j]
         }
+      }
       }
     }
     h=h+1
@@ -93,7 +117,5 @@ lotterie<-function(P,p,G,R){
   }
   
   colnames(res)<-c("Eleve","Groupe","Projet","Equipe-projet")
-  return(res)
+  return(res[,-4])
 }
-
-
